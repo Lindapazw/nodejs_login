6fc5475cf1ea519fe6e418f545c98243a9fce4c0
Linda Paz <lindacsndelapaz@gmail.com>
2023-01-31 09:31:13 -0300

    add middlewares

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const engine = require('ejs-mate');
-const path = require('path')
+const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -10,9 +11,11 @@ app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 
+// middlewares
+app.use(morgan('dev'));
+
 // routes
 app.use('/', require('./routes/index'));
-require('./routes/index');
 
 // starting the server
 app.listen(app.get('port'), () => {
