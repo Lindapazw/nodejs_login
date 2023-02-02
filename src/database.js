a3a56ad286eac125a5e5b338176d350cd8b8dd7c
Linda Paz <lindacsndelapaz@gmail.com>
2023-02-02 20:24:12 -0300

    fix useNewUrlParser

--- src/database.js
+++ src/database.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const {mongodb} = require('./keys');
 mongoose.set('strictQuery', true);
 
-mongoose.connect(mongodb.URI,{})
+mongoose.connect(mongodb.URI,{useNewUrlParser:true})
     .then(DB => console.log('Database is conected'))
     .catch(err => console.error(err));
