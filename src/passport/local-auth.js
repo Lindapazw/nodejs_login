3c673d503efc109ad70c05790c68a344d85583b9
Linda Paz <lindacsndelapaz@gmail.com>
2023-02-02 20:50:49 -0300

    add new user

--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -1,10 +1,12 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
+const User = require('../models/user')
+
 passport.use('local-signup', new LocalStrategy({
     usernameField: 'email',
     passwordField:'password',
     passReqToCallback: true
 }, (req, email, password, done) => {
-    
-}))
+    const user = new User(); // usuario objeto en blanco
+}));
