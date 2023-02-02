fdd86790676b833b330b3bf64e95bf107064df87
Linda Paz <lindacsndelapaz@gmail.com>
2023-02-02 20:19:53 -0300

    fix mongoose.set('strictQuery', true);

--- src/database.js
+++ src/database.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
+const {mongodb} = require('./keys');
+mongoose.set('strictQuery', true);
 
-mongoose.connect()
+mongoose.connect(mongodb.URI,{})
+    .then(DB => console.log('Database is conected'))
+    .catch(err => console.error(err));
