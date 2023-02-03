f6329c239851e75db4ba6c8ab3c6ca09f4f7a6ec
Linda Paz <lindacsndelapaz@gmail.com>
2023-02-03 10:06:25 -0300

    add  require('passport') in index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const engine = require('ejs-mate');
 const path = require('path');
 const morgan = require('morgan');
+const passport = require('passport');
 
 // Initializations
 const app = express();
 require('./database');
+require('./passport/local-auth');
 
 // settings
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +18,8 @@ app.set('port', process.env.PORT || 3000);
 // middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended:false}));
+app.use(passport.initialize()); // iniciamos passport
+app.use(passport.session());
 
 // routes
 app.use('/', require('./routes/index'));
