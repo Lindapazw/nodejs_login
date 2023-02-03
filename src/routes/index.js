635e4586f55605a1916d84968d24e8b851e2566d
Linda Paz <lindacsndelapaz@gmail.com>
2023-02-03 10:18:47 -0300

    add require passport in index/routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-
+const passport = require('passport'); // require passport
 
 router.get('/', (req, res, next) => {
     res.render('index');
