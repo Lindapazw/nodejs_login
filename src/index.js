38fa0fd1bd5002b452b5a60b152bd1b4eae24985
Linda Paz <lindacsndelapaz@gmail.com>
2023-02-05 11:01:14 -0300

    add coment

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const engine = require('ejs-mate');
-const path = require('path');
-const morgan = require('morgan');
+const express = require('express'); // exporto express
+const engine = require('ejs-mate'); // exporto ejs
+const path = require('path'); // exporto path
+const morgan = require('morgan'); // exporto morgan
 const passport = require('passport');
 const session = require('express-session');
 
 // Initializations
-const app = express();
+const app = express(); // denomino a express como app 
 require('./database');
 require('./passport/local-auth');
 
 // settings
-app.set('views', path.join(__dirname, 'views'));
-app.engine('ejs', engine);
-app.set('view engine', 'ejs');
-app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views')); // la carpeta de vista esta dentro de src 
+app.engine('ejs', engine); // utiliza el motor de plantillas ejs
+app.set('view engine', 'ejs'); // establece el motor de plantillas 
+app.set('port', process.env.PORT || 3000); // utiliza el puerto del sistema operativo o puerto 3000 
 
 // middlewares
-app.use(morgan('dev'));
+app.use(morgan('dev')); // utiliza morgan y da informacion en la terminal (por ejemplo refrescar pag)
 app.use(express.urlencoded({ extended:false}));
 app.use(session ({ 
     secret: 'myscretsession',
@@ -28,7 +28,7 @@ app.use(passport.initialize()); // iniciamos passport
 app.use(passport.session());
 
 // routes
-app.use('/', require('./routes/index'));
+app.use('/', require('./routes/index')); // express utiliza esta ruta cada vez que el usuario inicialize la app
 
 // starting the server
 app.listen(app.get('port'), () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); // require express
+const router = express.Router(); // passport devuelve el objeto router
 
 const passport = require('passport'); // require passport
 
-router.get('/', (req, res, next) => {
-    res.render('index');
+router.get('/', (req, res, next) => { // ventana de inicio
+    res.render('index'); // como respuesta al inicio de la app, se muestra la ventana index
 });
 
-router.get('/signup', (req, res, next) => {
+router.get('/signup', (req, res, next) => { // ventana registrarse
     res.render('signup')
 });
 
-router.post('/signup', passport.authenticate('local-signup', {
-    successRedirect:'/profile',
-    failureRedirect: '/signup',
+router.post('/signup', passport.authenticate('local-signup', { // una ves el usuario se registra, entra el post a esta ventana
+    successRedirect:'/profile', // si esta ok entra a perfil
+    failureRedirect: '/signup', // si no esta ok vuelve a registrarse
     passReqToCallback: true
 }));
 
-router.get('/signin', (req, res, next) => {
+router.get('/signin', (req, res, next) => { // ventana por la que el usuario vuelve a ingresar si ya esta registrado
 
 });
 
